604f3741bc9e48338e0eff7c15e04d0da7ae1df5
OcinS <nicodsison@gmail.com>
2023-12-13 21:24:33 +0800

    Update JS file

--- index.js
+++ index.js
@@ -8,21 +8,33 @@ function Book (title, author, pages, read) {
 };
 
 
+let titleValue = ``;
+let authorValue = ``;
+let pagesValue = ``;
+let readValue = ``;
 
 
-let readValue = document.getElementsByName(`#title`);
-
-let buttonAddBook = document.querySelector(`#addBookButton`);
-
-buttonAddBook.addEventListener(`click`, addBookToMyLibrary())
-
+const buttonAddBook = document.querySelector(`#addBookButton`);
+buttonAddBook.addEventListener(`click`, addBookToMyLibrary)
 
 function addBookToMyLibrary() {
+    
+    titleValue = document.querySelector(`#title`).value;
+    authorValue = document.querySelector(`#author`).value;
+    pagesValue = document.querySelector(`#pages`).value;
+    getRadioValue();
 
-    let titleValue = document.querySelector(`#title`).value;
-    let authorValue = document.querySelector(`#author`).value;
-    let pagesValue = document.querySelector(`#pages`).value;
-
+    
     myLibrary.push(new Book(titleValue, authorValue, pagesValue, readValue));
 
 };
+
+// This will get the Value of the Radio Buttons: Yes or No
+function getRadioValue() {
+    let radioValue = document.getElementsByName(`read-status`);
+    for (let i = 0; i < radioValue.length; i++) {
+        if (radioValue[i].checked) {
+            readValue = radioValue[i].value;
+        }
+    }
+}
